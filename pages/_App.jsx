7b48ee2c82aae5538c2ea2a5de4683c61adc31f8
GuiSilva011git config --global user.email guilherne1207@gmail.com <guilherne1207@gmail.com>
2023-01-30 20:52:04 -0300

    Importado a fonte e configurações de titulo

--- pages/_App.jsx
+++ pages/_App.jsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
+
 import '../src/global.css'
 
 function App({ Component,pageProps }){
-return <Component {...pageProps} />
+return ( <>
+       <Head>
+       <title>Titulo padrão</title>
+       </Head>
+       <Component {...pageProps} />
+</>
+  )
 }
 
 export default App
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,17 @@
+
 import Title from '../src/components/title/Title'
 import Subtitle, { NewSubtitle } from '../src/components/subtitle/Subtitle'
  
 function HomePage(){
  return(
+  
     <div>
-      
        <Title>Um titulo        </Title>
        <Subtitle  text="Um subtitulo"/>
        <Title>Mais um titulo   </Title>
        <Subtitle text="Outro subtitulo"/>
     </div>
+  
  )
    
 }
